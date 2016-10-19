479c07c7eb3b450b2ec851fb06e3299127152e78
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-10-19 22:33:42 +0200

    Add viewport meta tag

--- app/resources/views/layouts/head.blade.php
+++ app/resources/views/layouts/head.blade.php
@@ -1,6 +1,8 @@
 <head>
     <title>PastaBin</title>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/styles/solarized-dark.min.css">
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 </head>
