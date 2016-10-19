b655246af336cc6fe0029bc6b83b9bc35bb71802
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-10-19 19:11:03 +0200

    Change styling

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,46 +1,6 @@
 <head>
-    <title>Hi</title>
+    <title>Luna's PasteBin</title>
 
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/styles/solarized-dark.min.css">
-    <style>
-        html,
-        body,
-        pre {
-            background: #002b36;
-            padding: 0;
-            margin: 0;
-            height: 100%;
-        }
-
-        html {
-            box-sizing: border-box;
-        }
-        *, *:before, *:after {
-            box-sizing: inherit;
-        }
-
-        .content {
-            height: 100%;
-        }
-
-        pre > code {
-            padding: 0 !important;
-        }
-
-        .content div {
-            margin: -15px auto;
-            padding-left: 10px;
-        }
-
-        .content div:nth-of-type(odd) {
-            background: rgba(255,255,255,0.05);
-        }
-
-        .content div:before {
-            content: attr(data-prefix);
-            border-right: 1px solid rgba(255,255,255,0.4);
-            padding-right: 5px;
-            margin-right: 5px;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
--- resources/views/paste/show.blade.php
+++ resources/views/paste/show.blade.php
@@ -2,7 +2,6 @@
 
 @section('code')
     @foreach(explode("\n", $code) as $line)
-        <?php $prefix = str_pad($loop->iteration, (strlen($loop->count) < 3 ? 3 : strlen($loop->count)), " ", STR_PAD_LEFT); ?>
-<div data-prefix="{{ $prefix }}">{{ ($line ?: "&nbsp;") }}</div>
+<div data-prefix="{{ $loop->iteration }}">{{ $line }}&nbsp;</div>
     @endforeach
 @endsection
