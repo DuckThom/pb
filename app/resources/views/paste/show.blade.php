68f7f82980b2ed40bb10b2eaf98f0318fdef5c04
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-10-25 20:10:17 +0200

    Load lines from model

--- app/resources/views/paste/show.blade.php
+++ app/resources/views/paste/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('code')
-    @foreach($lines as $line)
+    @foreach($paste->getLines() as $line)
         <?php $prefix = (strlen($loop->count) < 5 ? str_pad($loop->iteration, 5, " ", STR_PAD_LEFT) : str_pad($loop->iteration, strlen($loop->count), " ", STR_PAD_LEFT)); ?>
 <div data-line="{{ $loop->iteration }}" data-prefix="{{ $prefix }}">{{ $line }}&nbsp;</div>
     @endforeach
